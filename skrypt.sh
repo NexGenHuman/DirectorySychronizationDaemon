dd408a844401ca4693306e5d10412ec6d6340c46
piotrgo00 <piotrgo2000@o2.pl>
2021-04-14 20:27:21 +0200

    skrypt do tworzenia katalogow

---
+++ skrypt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p ~/projektdemon/kat1/kat3/kat123
+mkdir -p ~/projektdemon/kat1/kat4
+mkdir -p ~/projektdemon/kat1/kat5
+mkdir -p ~/projektdemon/kat1/kat6
+mkdir -p ~/projektdemon/kat2/kat3
+mkdir -p ~/projektdemon/kat2/kat4
+mkdir -p ~/projektdemon/kat2/kat9
+
+echo "kot" > ~/projektdemon/kat1/kat3/plik1.txt
+echo "pies" > ~/projektdemon/kat1/kat3/plik2.txt
+echo "adad" > ~/projektdemon/kat1/kat4/plik213.txt
+echo "szszynszylka" > ~/projektdemon/kat1/kat5/pliczek.txt
+echo "kot" > ~/projektdemon/kat1/kat5/plik1.txt
+echo "misza" > ~/projektdemon/kat2/kat3/nieplik.txt
+echo "77777777" > ~/projektdemon/kat2/kat3/plikencja.txt
+echo "kotkotpiespies" > ~/projektdemon/kat2/kat3/pl1k.txt
+echo "mokotow" > ~/projektdemon/kat2/kat9/plik.txt
+echo "asddsasd" > ~/projektdemon/kat2/kat4/plikplik11.txt
+echo "kot" > ~/projektdemon/kat1/plik.txt
