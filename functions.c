64a730c9bdf84fd78a95143023c319d78a8e438f
NexGenHuman <konradwasilewski@vp.pl>
2021-04-14 21:00:43 +0200

    SwapBig prototype

--- functions.c
+++ functions.c
@@ -2,6 +2,8 @@
 #include "sys/stat.h"
 #include "time.h"
 #include "utime.h"
+#include "sys/mman.h"
+#include "fcntl.h"
 #define SMALL_FILE 1024*1024*64
 
 void Handler(char *nazwa)
@@ -43,5 +45,28 @@ void SwapSmall(char *path1, char *path2)
 
 void SwapBig(char *path1, char *path2)
 {
-    //Trochę już o mmap poczytałem i postaram się ja tą funkcję zrobić
+    struct stat filestat;
+    char* map1;
+    char* map2;
+
+    stat(path1, &filestat);
+
+    __off64_t size = &filestat.st_size;
+
+    int fd1 = open(path1, O_RDONLY);
+    int fd2 = open(path2, O_WRONLY);
+
+    map1 = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd1, 0);
+
+    truncate(path2, size);
+    map2 = mmap(NULL, size, PROT_WRITE, MAP_SHARED, fd2, 0);
+
+    for(int i = 0; i < size; i++)
+        map2[i]=map2[i];
+
+    munmap(fd1, size);
+    munmap(fd2, size);
+    
+    close(fd1);
+    close(fd2);
 }
