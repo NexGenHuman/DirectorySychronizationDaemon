5d5e11ae6db59110610659341a5c728c6d6bebbc
NexGenHuman <konradwasilewski@vp.pl>
2021-04-14 02:06:12 +0200

    Swap file basic code

--- functions.c
+++ functions.c
@@ -9,6 +9,8 @@ void Handler(char *nazwa)
     syslog(LOG_INFO, "Waking up daemon through SIGUSR1");
 }
 
+//REMEMBER! użyłem struct stat który jest tylko na Linux.
+
 //Return value true means that modification time of file in path1 is after modification time of path2
 bool CheckIfChanged(char *path1, char *path2)
 {
@@ -21,4 +23,25 @@ bool CheckIfChanged(char *path1, char *path2)
     if(difftime(ctime(&filestat1.st_mtime), ctime(&filestat2.st_mtime)) < 0)
         return false;
     return true;
+}
+
+//path2 is updated to path1 state
+void UpdateFile(char *path1, char *path2)
+{
+    struct stat filestat;
+
+    stat(path1, &filestat);
+    if(&filestat.st_size <= SMALL_FILE)
+        SwapSmall(path1, path2);
+    SwapBig(path1, path2);
+}
+
+void SwapSmall(char *path1, char *path2)
+{
+
+}
+
+void SwapBig(char *path1, char *path2)
+{
+    
 }
